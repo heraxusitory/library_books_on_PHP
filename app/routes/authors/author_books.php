f7ddcbcb94c1216ac04696b855a287baf5d4bfdf
heraxusitory <vladiksmart@gmail.com>
2020-10-27 19:44:11 +0300

    Указаны правильные заголовки в жанрах и авторах книг

--- app/routes/authors/author_books.php
+++ app/routes/authors/author_books.php
@@ -5,10 +5,12 @@ use App\Authors;
 $a = new Authors();
 $b = new Books();
 $idOfAuthor = $a->getIdOfAuthor();
+$authorList = $a->getAuthorById($_GET['author']);
+$authorName = $authorList['name'];
 
 if(checkExistAuthor($idOfAuthor)) {
 	 $booksAuthor = $b->getBooksById($_GET['author']);
-	 $title = strtoupper('Custom Author'); // TODO - указать правильного автора
+	 $title = mb_strtoupper($authorName); // TODO - указать правильного автора
 	include($_SERVER['DOCUMENT_ROOT'] . '/templates/author_books.php');
 
 } else {
--- app/routes/genres/genre_books.php
+++ app/routes/genres/genre_books.php
@@ -5,11 +5,13 @@ use App\Genre;
 $g = new Genre;
 $b = new Books();
 $idOfGenre = $g->getIdOfGenre();
+$genreList = $g->getGenreById($_GET['genre']);
+$genreName = $genreList['name'];
 
  if(checkExistGenre($idOfGenre)) {
 	 $booksGenre = $b->getGenreById($_GET['genre']);
 	//  TODO - указать правильный жанр
-	 $title = strtoupper('Custom genre');
+	 $title = mb_strtoupper($genreName);
 	include($_SERVER['DOCUMENT_ROOT'] . '/templates/genre_books.php');
 
  } else {
