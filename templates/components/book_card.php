6ab3a9963c32179c5fa26b77c16a43cc35f8c179
heraxusitory <vladiksmart@gmail.com>
2020-11-05 12:10:31 +0300

    Добавлены тернарные операторы, чтобы при следующем хите кнопки не перезагружались на старые значения

--- templates/components/book_card.php
+++ templates/components/book_card.php
@@ -5,14 +5,14 @@
         <p class="card-text">
             <div> <?= $book['author_name']; ?> </div>
         </p>
-        <a href="/?book=<?= $book['book_id'] ?>" class="btn btn-primary">Show</a>
-        <form method="POST" action="app/handlers/handler.php" class="form_favourite_add">
-        	 <input type="hidden" name="book_id" value="<?= $book['book_id'] ?>">
-             <input type="hidden" name="action" value="add">
-        	 <button class="btn btn-primary add-favourite">Добавить в избранное</button>
-             <div class="errorAdd"></div>
-        </form>
-
-       
+        <div class="btn_clc">
+            <a href="/?book=<?= $book['book_id'] ?>" class="btn btn-primary">Show</a>
+            <form method="POST" action="app/handlers/handler.php" class="<?= doesExistFavourite($book['book_id'], $userId)?'form_favourite_remove':'form_favourite_add'?>">
+            	 <input type="hidden" name="book_id" value="<?= $book['book_id'] ?>">
+                 <input type="hidden" name="action" value="<?= doesExistFavourite($book['book_id'], $userId)?'remove':'add'?>">
+            	 <button class="btn btn-primary <?= doesExistFavourite($book['book_id'], $userId)?'remove-favourite':'add-favourite'?>"><?= doesExistFavourite($book['book_id'], $userId)?'Delete from favourites':'Add to favourites'?></button>
+            </form>
+        </div>
+        <div class='fatal-error-favourite'></div>
     </div>
 </div>
