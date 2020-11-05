6c8de914c9a8f7d58990f234a7332a6eb9f039dc
Davut <dsergeeb@smartdatatech.io>
2020-11-05 14:02:24 +0300

    FIX route divided on front and back

--- app/routes/profile/favourites.php
+++ app/routes/profile/favourites.php
@@ -6,12 +6,5 @@ $b = new Books();
 $userId = $_SESSION['user']['user_id'];
 $books = $b->getBooks();
 $favourite = new Favourites;
-?>
-<div class="d-flex flex-wrap justify-content-between">
-    <?php  foreach($books as $book):
-    if($favourite->doesTheBookExistInFavourite($book['book_id'], $userId)):
-    	include($_SERVER['DOCUMENT_ROOT'] . '/templates/components/favourite_card.php');
-    endif; ?>
-    <?php endforeach;?>
-    	<h1 class='emptyFavourite'><?= $favourite->emptyFavouriteList($book['book_id'], $userId)?'There are no books in favourites':'' ?></h1>
-</div>
+
+include($_SERVER['DOCUMENT_ROOT'] . '/templates/favourites/list.php');
