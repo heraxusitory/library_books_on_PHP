772c8680dcdd01acf269f7356fa4eeb15dae60c1
heraxusitory <vladiksmart@gmail.com>
2020-11-05 12:04:12 +0300

    Добавлен класс Favourites и описаны методы для работы с добавлением книг в избранное

--- app/Favourites.php
+++ app/Favourites.php
@@ -3,8 +3,56 @@
 namespace App;
 
 class Favourites extends DB{
-	function isInFavourites($book_id, $user_id) {
+	private $getListFavouriteQuery = "SELECT * FROM favourites WHERE user_id = ";
+	private $addBookQuery = "INSERT INTO favourites (user_id, book_id) VALUES(:user_id,:book_id)";
+	function getListFavourite($user_id) {
+		$arrList = $this->getList($this->getListFavouriteQuery . $user_id);
+		return $arrList;
+	}
+
+	function addFavouriteBook($user_id, $book_id) {
+		$connect = $this->connect(DB_NAME, DB_USER, DB_PASSWORD);
+		$sth = $connect->prepare("INSERT INTO favourites (user_id, book_id) VALUES(:user_id,:book_id)");
+		$sth->bindParam(':user_id', $user_id);
+		$sth->bindParam(':book_id', $book_id);
+		$execute = $sth->execute();
+		if ($execute) {
+			return true;
+		} 
+		return false;
+	}
+
+	function dropFavourBook($book_id, $user_id) {
+		$connect = $this->connect(DB_NAME, DB_USER, DB_PASSWORD);
+		$sth = $connect->prepare("DELETE FROM favourites WHERE book_id = :book_id AND user_id = :user_id");
+		$sth->bindParam(':book_id', $book_id);
+		$sth->bindParam(':user_id', $user_id);
+		$execute = $sth->execute();
+		if ($execute) {
+			return true;
+		}
+		return false;
+	}
+
+	function doesTheBookExistInFavourite($book_id, $user_id) {
+		$listFavourite = $this->getListFavourite($user_id);
+		if (is_array($listFavourite)) {
+			for ($i=0; $i < count($listFavourite); $i++) { 
+				if (($listFavourite[$i]['book_id'] == $book_id) && ($listFavourite[$i]['user_id'] == $user_id)) {
+					return true;	
+				}
+			}
 		
+		}
+		return false;
+	}
+
+	function emptyFavouriteList($book_id, $user_id) {
+		$listFavourite =$this->getListFavourite($user_id);
+		if (empty($listFavourite)) {
+			return true;
+		}
+		return false;
 	}
 
 }
