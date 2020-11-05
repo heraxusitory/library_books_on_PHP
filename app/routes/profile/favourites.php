1b01899ff69de275b20035033972883b7e9e9427
heraxusitory <vladiksmart@gmail.com>
2020-11-05 12:14:32 +0300

    Добавлены тернарные операторы для проверки существования книг в избранном в целом и сама проверка существования избранных книг в БД и соотвествующее добавление их в раздел пользователя

--- app/routes/profile/favourites.php
+++ app/routes/profile/favourites.php
@@ -1,11 +1,17 @@
 <?php
 
 use App\Books;
+use App\Favourites;
 $b = new Books();
+$userId = $_SESSION['user']['user_id'];
 $books = $b->getBooks();
+$favourite = new Favourites;
 ?>
 <div class="d-flex flex-wrap justify-content-between">
-    <?php  foreach($books as $book): ?>
-        <?php include($_SERVER['DOCUMENT_ROOT'] . '/templates/components/favourite_card.php'); ?>
-    <?php endforeach; ?>
+    <?php  foreach($books as $book):
+    if($favourite->doesTheBookExistInFavourite($book['book_id'], $userId)):
+    	include($_SERVER['DOCUMENT_ROOT'] . '/templates/components/favourite_card.php');
+    endif; ?>
+    <?php endforeach;?>
+    	<h1 class='emptyFavourite'><?= $favourite->emptyFavouriteList($book['book_id'], $userId)?'There are no books in favourites':'' ?></h1>
 </div>
--- templates/components/favourite_card.php
+++ templates/components/favourite_card.php
@@ -1,11 +1,19 @@
-<div class="card mt-4" style="width: 21rem;">
+<div class="card mt-4 favourite-drop-<?= $book['book_id'] ?>" style="width: 21rem;">
     <div class="img_block"></div>
     <div class="card-body">
         <h5 class="card-title"><?= $book['book_name']; ?></h5>
         <p class="card-text">
             <div> <?= $book['author_name']; ?> </div>
         </p>
-        <a href="/?book=<?= $book['book_id'] ?>" class="btn btn-primary">Show</a>
-        <a href="#" class="btn btn-primary">Убрать из избранного</a>
+       	<div class='btn_clc'>
+	        <a href="/?book=<?= $book['book_id'] ?>" class="btn btn-primary">Show</a>
+	        <form method="POST" action="app/handlers/handler.php" class="form_favourite_remove">
+	        	 <input type="hidden" name="book_id" value="<?= $book['book_id'] ?>">
+	             <input type="hidden" name="action" value="remove">
+	             <input type="hidden" name="dropFavourite" value='true'>
+	        	 <button class="btn btn-primary remove-favourite">Delete from favourites</button>
+	        </form>
+	    </div>
+       
     </div>
 </div>
