403e87a37bb56b6a524828817d0db2e9b992f324
heraxusitory <vladiksmart@gmail.com>
2020-11-05 12:06:57 +0300

    добавлена дополнительная обработка для кнопок и надписи с ошибками

--- app/handlers/handler.php
+++ app/handlers/handler.php
@@ -3,80 +3,82 @@
 require($_SERVER['DOCUMENT_ROOT'] . '/app/config.php');
 require($_SERVER['DOCUMENT_ROOT'] . '/app/init.php');
 
+use App\Favourites;
+$favourite = new Favourites;
+$userId = $_SESSION['user']['user_id'];
+$bookId = $_REQUEST['book_id'];
+//Проверка на нажаитие кнопки submit
 if (!empty($_REQUEST)) {
-	if ($_REQUEST['action'] == 'add') {
-		if (isset($_SESSION['user'])) {
-
-			$arResponse['user_id'] = $_SESSION['user']['user_id'];
-			$arResponse['book_id'] = $_REQUEST['book_id'];
-			$arResponse['action'] = 'add';
-			$arResponse['removeAction'] = 'remove';
-			$arResponse['text'] = 'Удалить из избранного';
-			// echo "<pre>";
-			$arrFavBooks = $db->getList("SELECT book_id FROM favourites");
-			// var_dump($arrFavBooks);
-			if (!empty($arrFavBooks)) {
-				$errorAdd = false;
-				for ($i=0; $i <count($arrFavBooks) ; $i++) { 
-					if ($arrFavBooks[$i]['book_id'] == $_REQUEST['book_id']) {
-						$errorAdd = true;
-						$arResponse['errorAdd'] = $errorAdd;
-						break;
+	//Проверяем авторизован ли пользователь
+	if (isset($_SESSION['user'])) {
+		if (!($favourite->addFavouriteBook($userId, $bookId)) || !($favourite->dropFavourBook($bookId, $userId))) {
+			$arResponse['fatalError'] = true;
+			$arResponse['book_id'] = $bookId;
+		} else {
+			$arResponse['isAuth'] = true;
+			//Проверяем как кнопка нажата(в данном случае проверка на нажатие 'Добавить в избранное'')
+			if ($_REQUEST['action'] == 'add') {
+				$arResponse['user_id'] = $_SESSION['user']['user_id'];
+				$arResponse['book_id'] = $_REQUEST['book_id'];
+				$arResponse['action'] = 'add';
+				$arResponse['removeAction'] = 'remove';
+				$arResponse['text'] = 'Delete from favourites';
+				//Записываем в массив все записи из таблицы БД favourites
+				$arrFavBooks = $favourite->getListFavourite($userId);
+				//Если таблица БД не пустая,то проверяем нет ли уже такой добавляемой книги в записи таблицы БД
+				if (!empty($arrFavBooks)) {
+					$errorAdd = false;
+					for ($i=0; $i <count($arrFavBooks) ; $i++) {
+						//Если существует такая книга, то вспомогательный флаг об ошибке добавления
+						if ($arrFavBooks[$i]['book_id'] == $_REQUEST['book_id'] && $arrFavBooks[$i]['user_id'] == $userId) {
+							$errorAdd = true;
+							$arResponse['errorAdd'] = $errorAdd;
+							break;
+						}
 					}
-				}
 
-				if (!$errorAdd) {
-						$sth = $connect->prepare("INSERT INTO favourites (user_name, book_id) VALUES(:user_name,:book_id)");
-						$sth->bindParam(':user_name', $userName);
-						$sth->bindParam(':book_id', $bookId);
-						$userName = $_SESSION['user']['user_id'];
-						// echo "<pre>";
-						$bookId = $_REQUEST['book_id'];
-						$sth->execute();
-
+					if (!$errorAdd) {
+							$favourite->addFavouriteBook($userId, $bookId);
+						}
+				} else {
+						$favourite->addFavouriteBook($userId, $bookId);
 					}
-			} else {
-					$sth = $connect->prepare("INSERT INTO favourites (user_name, book_id) VALUES(:user_name,:book_id)");
-					$sth->bindParam(':user_name', $userName);
-					$sth->bindParam(':book_id', $bookId);
-					$userName = $_SESSION['user']['user_id'];
-					// echo "<pre>";
-					$bookId = $_REQUEST['book_id'];
-					$sth->execute();
-				}
-			
+			}
 
-			
-			
-
-		}
-	}
-	if ($_REQUEST['action'] == 'remove') {
-		if (isset($_SESSION['user'])) {
-			$arResponse['user_id'] = $_SESSION['user']['user_id'];
-			$arResponse['book_id'] = $_REQUEST['book_id'];
-			$arResponse['action'] = 'remove';
-			$arResponse['removeAction'] = 'add';
-			$arResponse['text'] = 'Добавить в избранное';
-
-			$arrFavBooks = $db->getList("SELECT book_id FROM favourites");
-			// var_dump($arrFavBooks);
-			$errorAdd = false;
-			for ($i=0; $i <count($arrFavBooks) ; $i++) { 
-				if ($arrFavBooks[$i]['book_id'] == $_REQUEST['book_id']) {
-					$sth = $connect->prepare("DELETE FROM favourites WHERE book_id = :id");
-					$sth->bindParam(':id', $bookId);
-					$bookId = $_REQUEST['book_id'];
-					$sth->execute();
-
-					$errorRemove = true;
-					$arResponse['errorRemove'] = $errorRemove;
-					break;
+			if (($_REQUEST['action'] == 'remove') && ($_REQUEST['dropFavourite'] == 'true')) {
+				$arResponse['dropFavourite'] = true;
+				$arResponse['book_id'] = $_REQUEST['book_id'];
+				$arResponse['action'] = 'remove';
+				$favourite->dropFavourBook($bookId, $userId);
+				if ($favourite->emptyFavouriteList($bookId, $userId)) {
+					$arResponse['emptyFavourite'] = true;
 				}
 			}
-		}	
 			
+			else if ($_REQUEST['action'] == 'remove') {
+					$arResponse['user_id'] = $_SESSION['user']['user_id'];
+					$arResponse['book_id'] = $_REQUEST['book_id'];
+					$arResponse['action'] = 'remove';
+					$arResponse['removeAction'] = 'add';
+					$arResponse['text'] = 'Add to favourites';
+
+					$arrFavBooks = $favourite->getListFavourite($userId);
+					$errorAdd = false;
+					for ($i=0; $i <count($arrFavBooks) ; $i++) { 
+						if ($arrFavBooks[$i]['book_id'] == $_REQUEST['book_id'] && $arrFavBooks[$i]['user_id'] == $userId) {
+							$favourite->dropFavourBook($bookId, $userId);
+							$errorRemove = true;
+							$arResponse['errorRemove'] = $errorRemove;
+							break;
+						}
+					}	
+			}
+		}
+		
+	} else {
+		$arResponse['isAuth'] =false;
 	}
+	
 }
 
 echo json_encode($arResponse);
