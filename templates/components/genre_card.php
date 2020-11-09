8c596adf3593f934ea3948b04c7503e5b33a0a61
heraxusitory <vladiksmart@gmail.com>
2020-11-09 08:58:31 +0300

    Редактированы гет-параметры в ссылке и добавлен класс target

--- templates/components/genre_card.php
+++ templates/components/genre_card.php
@@ -2,6 +2,6 @@
     <div class="img_block"></div>
     <div class="card-body">
         <h5 class="card-title"><?= $genre['name']; ?></h5>
-        <a href="/?genre=<?= $genre['id'] ?>" class="btn btn-primary">Show books</a>
+        <a href="/?page=genre&id=<?= $genre['id'] ?>" class="btn btn-primary target">Show books</a>
     </div>
 </div>
