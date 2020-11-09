e2fc4f33df9153d34fe35cbd14ca13c011729d28
heraxusitory <vladiksmart@gmail.com>
2020-11-09 08:43:43 +0300

    Удалено подключение роутеров

--- index.php
+++ index.php
@@ -2,12 +2,15 @@
 require($_SERVER['DOCUMENT_ROOT'] . '/templates/header.php');
 ?>
     <div class="container custom-container">
+    	<div id="content">
+	
+		</div>
         <?php 
-        if (file_exists($_SERVER['DOCUMENT_ROOT'] . '/app/router.php')){
+        /*if (file_exists($_SERVER['DOCUMENT_ROOT'] . '/app/router.php')){
             require($_SERVER['DOCUMENT_ROOT'] . '/app/router.php');
         } else {
             die("Nothing to show. Something's bad happend :(");
-        }       
+        }*/       
         ?>
     </div>
 
