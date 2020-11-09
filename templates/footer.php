dcdf410d2eb4f67aeb51db98692b37b14b834fd1
heraxusitory <vladiksmart@gmail.com>
2020-11-09 09:00:02 +0300

    Добавлено подключение нескольких скриптов, работающих на переход по страницам без перезагрузки

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,8 @@
 </main>
     <footer>
     </footer>
+    <script type="text/javascript" src="/assets/js/helpers.js"></script>
+    <script type="text/javascript" src="/assets/js/main.js"></script>
     <script type="text/javascript" src="/assets/js/script.js"></script>
     <script type="text/javascript" src="/assets/js/favourite.js"></script>
     </body>
