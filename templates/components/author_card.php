67932fc05fbc99ded29bbebaec933a2649fffa0f
heraxusitory <vladiksmart@gmail.com>
2020-11-09 08:47:27 +0300

    Редактирование гет-параметров в ссылке и добавление класса target

--- templates/components/author_card.php
+++ templates/components/author_card.php
@@ -2,6 +2,6 @@
     <div class="img_block"></div>
     <div class="card-body">
         <h5 class="card-title"><?= $author['name']; ?></h5>
-        <a href="/?author=<?= $author['id'] ?>" class="btn btn-primary">Show own books</a>
+        <a href="/?page=author&id=<?= $author['id'] ?>" class="btn btn-primary target">Show own books</a>
     </div>
 </div>
