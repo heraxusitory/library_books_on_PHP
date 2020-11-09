df970eaf071c4efed995d1c74f9327cbb13a28bb
heraxusitory <vladiksmart@gmail.com>
2020-11-09 08:56:31 +0300

    Редактированы гетр-параметры в ссылке

--- templates/components/genre_books.php
+++ templates/components/genre_books.php
@@ -5,6 +5,6 @@
         <p class="card-text">
             <div> <?= $book['author_name']; ?> </div>
         </p>
-         <a href="/?book=<?= $book['book_id'] ?>" class="btn btn-primary">Show</a>
+         <a href="/?page=book&id=<?= $book['book_id'] ?>" class="btn btn-primary">Show</a>
     </div>
 </div>
